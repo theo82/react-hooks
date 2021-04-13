5d800d4ea00371db2b755e643616e119fa0763ac
theo82 <theoaristi53@gmail.com>
2021-04-13 17:57:37 +0300

    useState hook

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import ClassCounter from './components/ClassCounter';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <ClassCounter />
+    </div>
+  );
 }
 
 export default App;
