59a64a321e17f68d8b05cefbc63d005f87b946cf
theo82 <theoaristi53@gmail.com>
2021-04-14 09:16:27 +0300

    useState with arrays

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 // import ClassCounter from './components/ClassCounter';
 // import HookCounter from './components/HookCounter';
 // import HookCounterTwo from './components/HookCounterTwo';
-import HookCounterThree from './components/useState/HookCounterThree';
+// import HookCounterThree from './components/useState/HookCounterThree';
+import HookCounterFour from './components/useState/HookCounterFour';
+
 function App() {
   return (
     <div className='App'>
       {/* <ClassCounter /> */}
-      <HookCounterThree />
+      <HookCounterFour />
     </div>
   );
 }
