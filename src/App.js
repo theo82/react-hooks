b8fb7d450159043c03b0936fd20bee9decec4a8f
theo82 <theoaristi53@gmail.com>
2021-04-14 08:15:26 +0300

    useState with object

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import ClassCounter from './components/ClassCounter';
-import HookCounter from './components/HookCounter';
-import HookCounterTwo from './components/HookCounterTwo';
-
+// import ClassCounter from './components/ClassCounter';
+// import HookCounter from './components/HookCounter';
+// import HookCounterTwo from './components/HookCounterTwo';
+import HookCounterThree from './components/useState/HookCounterThree';
 function App() {
   return (
     <div className='App'>
       {/* <ClassCounter /> */}
-      <HookCounterTwo />
+      <HookCounterThree />
     </div>
   );
 }
