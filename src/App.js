65acb561e5cb048ab3132af70e79817a524d6d19
theo82 <theoaristi53@gmail.com>
2021-04-22 10:10:24 +0300

    Fetching Data with useEffect Part 1

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 // import ClassCounter from './components/ClassCounter';
 // import HookCounter from './components/HookCounter';
-// import HookCounterTwo from './components/HookCounterTwo';
-// import HookCounterThree from './components/useState/HookCounterThree';
+// import HookCounterTwo from './components/useState/HookCounterTwo';
+//import HookCounterThree from './components/useState/HookCounterThree';
 // import HookCounterFour from './components/useState/HookCounterFour';
-import ClassCounterOne from './components/useEffect/ClassCounterOne';
-import HookCounterOne from './components/useEffect/HookCounterOne';
+// import ClassCounterOne from './components/useEffect/ClassCounterOne';
+// import HookCounterOne from './components/useEffect/HookCounterOne';
+import DataFetching from './components/useEffect/fetching/DataFetching';
 
 function App() {
   return (
     <div className='App'>
+      {/* <HookCounterThree /> */}
       {/* <ClassCounter /> */}
       {/* <ClassCounterOne /> */}
-      <HookCounterOne />
+      <DataFetching />
     </div>
   );
 }
--- src/components/useState/HookCounterThree.js
+++ src/components/useState/HookCounterThree.js
@@ -4,33 +4,25 @@ function HookCounterThree() {
   const [name, setName] = useState({ firstName: '', lastName: '' });
 
   return (
-    <div>
-      <form>
-        <input
-          type='text'
-          name={name.firstName}
-          onChange={(e) =>
-            setName({
-              ...name,
-              firstName: e.target.value,
-            })
-          }
-        />
-        <input
-          type='text'
-          name={name.lastName}
-          onChange={(e) =>
-            setName({
-              ...name,
-              lastName: e.target.value,
-            })
-          }
-        />
-        <h2>Your first name is: {name.firstName}</h2>
-        <h2>Your last name is: {name.lastName}</h2>
-        <h2>{JSON.stringify(name)}</h2>
-      </form>
-    </div>
+    <form>
+      <input
+        type='text'
+        value={name.firstName}
+        // // The useState doesnt' automatically merge and update the object
+        // onChange={(e) => setName({ firstName: e.target.value })}
+        onChange={(e) => setName({ ...name, firstName: e.target.value })}
+      />
+      <input
+        type='text'
+        value={name.lastName}
+        // The useState doesnt' automatically merge and update the object
+        // onChange={(e) => setName({ firstName: e.target.value })}
+        onChange={(e) => setName({ ...name, lastName: e.target.value })}
+      />
+      <h2>Your first name is - {name.firstName}</h2>
+      <h2>Your second name is - {name.lastName}</h2>
+      <h2>{JSON.stringify(name)}</h2>
+    </form>
   );
 }
 
