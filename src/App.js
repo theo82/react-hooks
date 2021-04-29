4e76d3231039a7bdcf3287b3a749a68f4a38bbe7
theo82 <theoaristi53@gmail.com>
2021-04-29 11:33:13 +0300

    useReducer with useContext

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useReducer } from 'react';
 // import ClassCounter from './components/ClassCounter';
 // import HookCounter from './components/HookCounter';
 // import HookCounterTwo from './components/useState/HookCounterTwo';
@@ -12,26 +11,55 @@ import React from 'react';
 // import ComponentC from './components/useContext/ComponentC';
 // import CounterOne from './components/useReducer/CounterOne';
 // import CounterTwo from './components/useReducer/CounterTwo';
-import CounterThree from './components/useReducer/CounterThree';
+// import CounterThree from './components/useReducer/CounterThree';
+import ComponentA from './components/useReducer/GlobalStateManagment/ComponentA';
+import ComponentB from './components/useReducer/GlobalStateManagment/ComponentB';
+import ComponentC from './components/useReducer/GlobalStateManagment/ComponentC';
 
-export const UserContext = React.createContext();
-export const ChannelContext = React.createContext();
+export const CountContext = React.createContext();
+
+// export const UserContext = React.createContext();
+// export const ChannelContext = React.createContext();
+
+const initialState = 0;
+
+const reducer = (state, action) => {
+  switch (action) {
+    case 'increment':
+      return state + 1;
+    case 'decrement':
+      return state - 1;
+    case 'reset':
+      return initialState;
+    default:
+      return state;
+  }
+};
 
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState);
   return (
-    <div className='App'>
-      {/* <HookCounterThree /> */}
-      {/* <ClassCounter /> */}
-      {/* <ClassCounterOne /> */}
-      {/* <DataFetching /> */}
-      {/* <UserContext.Provider value={'Theo'}>
-        <ChannelContext.Provider value={'CodeEvolution'}>
-          <ComponentC />
-        </ChannelContext.Provider>
-      </UserContext.Provider> */}
-      {/* <CounterTwo /> */}
-      <CounterThree />
-    </div>
+    <CountContext.Provider
+      value={{ countState: count, countDispatch: dispatch }}
+    >
+      <div className='App'>
+        {/* <HookCounterThree /> */}
+        {/* <ClassCounter /> */}
+        {/* <ClassCounterOne /> */}
+        {/* <DataFetching /> */}
+        {/* <UserContext.Provider value={'Theo'}>
+          <ChannelContext.Provider value={'CodeEvolution'}>
+            <ComponentC />
+          </ChannelContext.Provider>
+        </UserContext.Provider> */}
+        {/* <CounterTwo /> */}
+        {/* <CounterThree /> */}
+        Count - {count}
+        <ComponentA />
+        <ComponentB />
+        <ComponentC />
+      </div>
+    </CountContext.Provider>
   );
 }
 
